198e9e23dec3c9862ce2ce5a1c46f11c0781b079
trumpocalyspe <mark.jn.kim@gmail.com>
2018-09-11 12:35:52 -0700

    added api for youtube videos

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const VideoList = (
-  <div />
-)
+const VideoList = props => (
+ 
+ <ul className="col-me-4 list-group">
+  {props.videos.length}
+  </ul>
+ )
+
 
 export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/videoList'
 import SearchBar from './components/searchBar';
 const API_KEY = 'AIzaSyB_QRokAvIBJXCjI0pG_XC1IxAEKbv3dZM';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data)
-)
+
 // Create a new component. This component should produce some HTML
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => { this.setState({ videos })
+    });
+  }
+  
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
